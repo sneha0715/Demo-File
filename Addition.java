e2f0fc7a9905d6cf482f917a9b8147eef4e12069
sneha0715 <157895540+sneha0715@users.noreply.github.com>
2026-01-13 16:14:16 +0530

    b

--- Addition.java
+++ Addition.java
@@ -1,10 +1,8 @@
-package addition;
-
 public class Addition {
   public static void main(String[] args) {
-    int a = 5;
-    int b = 10;
-    int sum = a + b;
+    int a = 2;
+    int b = 14;
+    int sum = a - b;
 
     System.out.println("The sum of " + a + " and " + b + " is: " + sum);
   }
